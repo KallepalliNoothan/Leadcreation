9b325c3863e3055a80abfddb76811fdac4c9afd0
Noothan K <kallepallinoothan@gmail.com>
2025-06-20 12:06:38 +0530

    Stopping the submit function and navigation if re-captcha isn't verified

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
+let captchavalue = false;
 function beforeSubmit() {
-    let outputdate = document.querySelector(".outputdate");
-    let inputdate = document.querySelector(".inputdate");
-    console.log("Input Date:", inputdate.value);  
-    let formatteddate = new Date(inputdate.value).toLocaleDateString("en-IN");
-   outputdate.value = formatteddate;
-    console.log("Formatted Date:", outputdate.value);
+
+    if(captchavalue = true){
+        let outputdate = document.querySelector(".outputdate");
+        let inputdate = document.querySelector(".inputdate");
+        console.log("Input Date:", inputdate.value);  
+        let formatteddate = new Date(inputdate.value).toLocaleDateString("en-IN");
+        outputdate.value = formatteddate;
+        console.log("Formatted Date:", outputdate.value);
+    }
+    else {
+        alert("Please complete the CAPTCHA before submitting the form.");
+        event.preventDefault();
+    }
+  
 }
 
 function timestamp() { var response = document.getElementById("g-recaptcha-response"); if (response == null || response.value.trim() == "") {var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);elems["ts"] = JSON.stringify(new Date().getTime());document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); } } setInterval(timestamp, 500); 
+
+function captchaChecked() {
+    captchavalue = true;
+}
