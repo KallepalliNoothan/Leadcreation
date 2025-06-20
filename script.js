2fa912a20af1ef812ba005d188a9f1c042e83346
Noothan K <kallepallinoothan@gmail.com>
2025-06-20 12:23:05 +0530

    small alterations

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let captchavalue = false;
 function beforeSubmit() {
 
-    if(captchavalue = true){
+    if(captchavalue){
         let outputdate = document.querySelector(".outputdate");
         let inputdate = document.querySelector(".inputdate");
         console.log("Input Date:", inputdate.value);  
@@ -16,7 +16,13 @@ function beforeSubmit() {
   
 }
 
-function timestamp() { var response = document.getElementById("g-recaptcha-response"); if (response == null || response.value.trim() == "") {var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);elems["ts"] = JSON.stringify(new Date().getTime());document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); } } setInterval(timestamp, 500); 
+function timestamp() { 
+    var response = document.getElementById("g-recaptcha-response");
+     if (response == null || response.value.trim() == "") {
+        var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);elems["ts"] = JSON.stringify(new Date().getTime());document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); 
+    } 
+} 
+setInterval(timestamp, 500); 
 
 function captchaChecked() {
     captchavalue = true;
